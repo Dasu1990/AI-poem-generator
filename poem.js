b8e9034fa34ba9327a82b6d7bddc4c6e83a799af
Dasu1990 <dasuendalek12@gmail.com>
2024-12-26 14:16:30 +0300

    added API key

--- poem.js
+++ poem.js
@@ -1,14 +1,23 @@
-function generate(event) {
-    event.preventDefault();
-    //let poemGen = document.querySelector("#poem");
-    
+
+function display(response) {    
+
     new Typewriter("#poem", {
-        strings: "A flower blooms, brief but bright a gentle spark of fleeting light",
+        strings:  response.data.answer,
         autoStart: true,
         cursor: null,
         delay: 20,
     });
 }
-
+function generate(event) {
+    event.preventDefault();
+    let topic= document.querySelector("#topic")
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "Your are a good AI Assistant that writes short and sweet poem. The poem must be provided in HTML format. Example: <p>this is a poem</p> make sure to follow user instraction";
+  let prompt = `user instruction: Generate a short and unique poem about ${topic.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("called the AI api");
+  axios.get(apiUrl).then(display);
+}
 let poem = document.querySelector("#poem-generator");
 poem.addEventListener("submit", generate);
